20197cd263d20dda307637224ee0f4097770d72b
Viacheslav <viacheslav@mcublog.xyz>
2020-04-07 22:44:45 +0300

    Added arguments parse; Cleanup; Formatting;

--- LibSawAnalyzing/LibSawAnalyzing.cpp
+++ LibSawAnalyzing/LibSawAnalyzing.cpp
@@ -6,121 +6,121 @@
 
 namespace SawAnalyzeFuncs
 {
-	SawAnalyzingFunc::AMPLITUDE_ERRORS SawAnalyzingFunc::amplitude_checking(const TEST_PARAM &tp, RESULT &res, vector<int> &data)
-	{
-		int min_trh = 0, max_trh = 0;
+    SawAnalyzingFunc::AMPLITUDE_ERRORS SawAnalyzingFunc::amplitude_checking(const TEST_PARAM &tp, RESULT &res, vector<int> &data)
+    {
+        int min_trh = 0, max_trh = 0;
 
-		res.ampl_test = AMPL_NORM;
+        res.ampl_test = AMPL_NORM;
 
-		std::vector<int>::iterator a_max = max_element(data.begin(), data.end());
-		std::vector<int>::iterator a_min = min_element(data.begin(), data.end());
-		
-		if (!(*a_min >= tp.aml_min - tp.err && *a_min <= tp.aml_min + tp.err))
-		{
-			res.ampl_test = AMPL_TOO_LOW;
-			res.aml_min_err = *a_min;
-		}
+        std::vector<int>::iterator a_max = max_element(data.begin(), data.end());
+        std::vector<int>::iterator a_min = min_element(data.begin(), data.end());
 
-		if (!(*a_max >= tp.aml_max - tp.err && *a_max <= tp.aml_max + tp.err))
-		{
-			if (res.ampl_test == AMPL_TOO_LOW)
-				res.ampl_test = AMPL_OUT_BORDER;
-			else 
-				res.ampl_test = AMPL_TOO_HIGHT;
+        if (!(*a_min >= tp.aml_min - tp.err && *a_min <= tp.aml_min + tp.err))
+        {
+            res.ampl_test = AMPL_TOO_LOW;
+            res.aml_min_err = *a_min;
+        }
 
-			res.aml_max_err = *a_max;
-		}
+        if (!(*a_max >= tp.aml_max - tp.err && *a_max <= tp.aml_max + tp.err))
+        {
+            if (res.ampl_test == AMPL_TOO_LOW)
+                res.ampl_test = AMPL_OUT_BORDER;
+            else
+                res.ampl_test = AMPL_TOO_HIGHT;
 
-		return res.ampl_test;
-	}
+            res.aml_max_err = *a_max;
+        }
 
-	bool SawAnalyzingFunc::pulse_width_checking(const TEST_PARAM &tp, RESULT &res, vector<int> &data)
-	{
-		res.widht_test = false;
-		res.width_err = data.size();
-		if (tp.width == res.width_err)
-			res.widht_test = true;
+        return res.ampl_test;
+    }
 
-		return res.widht_test;
-	}
+    bool SawAnalyzingFunc::pulse_width_checking(const TEST_PARAM &tp, RESULT &res, vector<int> &data)
+    {
+        res.widht_test = false;
+        res.width_err = data.size();
+        if (tp.width == res.width_err)
+            res.widht_test = true;
 
-	bool SawAnalyzingFunc::form_checking(RESULT &res, vector<int> &data)
-	{
-		res.form_tst = true;
-		for (int i = 0; i < data.size() - 1; i++)
-		{
-			if (data[i] >= data[i + 1])
-			{
-				res.bad_sample_num = i;
-				res.form_tst = false;
-				break;
-			}
-		}		
-		return res.form_tst;
-	}
+        return res.widht_test;
+    }
 
-	bool SawAnalyzingFunc::check_all_tests(const TEST_PARAM &tp, RESULT &res, vector<int> &data)
-	{
-		form_checking(res, data);
-		pulse_width_checking(tp, res, data);
-		amplitude_checking(tp, res, data);
+    bool SawAnalyzingFunc::form_checking(RESULT &res, vector<int> &data)
+    {
+        res.form_tst = true;
+        for (int i = 0; i < data.size() - 1; i++)
+        {
+            if (data[i] >= data[i + 1])
+            {
+                res.bad_sample_num = i;
+                res.form_tst = false;
+                break;
+            }
+        }
+        return res.form_tst;
+    }
 
-		return result_check(res);
-	}
+    bool SawAnalyzingFunc::check_all_tests(const TEST_PARAM &tp, RESULT &res, vector<int> &data)
+    {
+        form_checking(res, data);
+        pulse_width_checking(tp, res, data);
+        amplitude_checking(tp, res, data);
 
-	bool SawAnalyzingFunc::result_check(RESULT &res)
-	{
-		if (res.ampl_test == AMPL_NORM && res.form_tst && res.widht_test)
-			return true;
-		return false;
-	}
+        return result_check(res);
+    }
 
-	std::ostream& operator<<(std::ostream& out, const SawAnalyzingFunc::AMPLITUDE_ERRORS err)
-	{
-		static std::map<SawAnalyzingFunc::AMPLITUDE_ERRORS, std::string> strings;
-		if (strings.size() == 0)
-		{
+    bool SawAnalyzingFunc::result_check(RESULT &res)
+    {
+        if (res.ampl_test == AMPL_NORM && res.form_tst && res.widht_test)
+            return true;
+        return false;
+    }
+
+    std::ostream& operator<<(std::ostream& out, const SawAnalyzingFunc::AMPLITUDE_ERRORS err)
+    {
+        static std::map<SawAnalyzingFunc::AMPLITUDE_ERRORS, std::string> strings;
+        if (strings.size() == 0)
+        {
 #define INSERT_ELEMENT(p) strings[p] = #p
-			INSERT_ELEMENT(SawAnalyzingFunc::AMPL_NORM);
-			INSERT_ELEMENT(SawAnalyzingFunc::AMPL_TOO_LOW);
-			INSERT_ELEMENT(SawAnalyzingFunc::AMPL_TOO_HIGHT);
-			INSERT_ELEMENT(SawAnalyzingFunc::AMPL_OUT_BORDER);
+            INSERT_ELEMENT(SawAnalyzingFunc::AMPL_NORM);
+            INSERT_ELEMENT(SawAnalyzingFunc::AMPL_TOO_LOW);
+            INSERT_ELEMENT(SawAnalyzingFunc::AMPL_TOO_HIGHT);
+            INSERT_ELEMENT(SawAnalyzingFunc::AMPL_OUT_BORDER);
 #undef INSERT_ELEMENT
-		}
-		return out << strings[err].c_str();
-	}
+        }
+        return out << strings[err].c_str();
+    }
 
-	void SawAnalyzingFunc::result_out(RESULT &res)
-	{
-		std::cout << "FormTest," << res.form_tst;
-		if (!res.form_tst)
-			std::cout << ",BadSample," << res.bad_sample_num;
-		std::cout << '\n';
+    void SawAnalyzingFunc::result_out(RESULT &res)
+    {
+        std::cout << "FormTest," << res.form_tst;
+        if (!res.form_tst)
+            std::cout << ",BadSample," << res.bad_sample_num;
+        std::cout << '\n';
 
-		std::cout << "AmplTest," << res.ampl_test;
-		if (res.ampl_test == AMPL_TOO_LOW)
-			std::cout << ",A_MIN_CURRENT," << res.aml_min_err;
-		else if (res.ampl_test == AMPL_TOO_HIGHT)
-			std::cout << ",A_MAX_CURRENT," << res.aml_max_err;
-		else if (res.ampl_test == AMPL_OUT_BORDER)
-		{
-			std::cout << ",A_MIN_CURRENT," << res.aml_min_err;
-			std::cout << ",A_MAX_CURRENT," << res.aml_max_err;
-		}
-		std::cout << '\n';
+        std::cout << "AmplTest," << res.ampl_test;
+        if (res.ampl_test == AMPL_TOO_LOW)
+            std::cout << ",A_MIN_CURRENT," << res.aml_min_err;
+        else if (res.ampl_test == AMPL_TOO_HIGHT)
+            std::cout << ",A_MAX_CURRENT," << res.aml_max_err;
+        else if (res.ampl_test == AMPL_OUT_BORDER)
+        {
+            std::cout << ",A_MIN_CURRENT," << res.aml_min_err;
+            std::cout << ",A_MAX_CURRENT," << res.aml_max_err;
+        }
+        std::cout << '\n';
 
-		std::cout << "WidthTest," << res.widht_test;
-		if (!res.widht_test)
-			std::cout << ",WIDTH_CURRENT," << res.width_err;
-		std::cout << '\n';
-	}
+        std::cout << "WidthTest," << res.widht_test;
+        if (!res.widht_test)
+            std::cout << ",WIDTH_CURRENT," << res.width_err;
+        std::cout << '\n';
+    }
 
-	void SawAnalyzingFunc::test_param_out(const TEST_PARAM &tp)
-	{
-		std::cout << "TestParameters\n";
-		std::cout << "AMPLITUDE_MIN," << tp.aml_min << '\n';
-		std::cout << "AMPLITUDE_MAX," << tp.aml_max << '\n';
-		std::cout << "AMPLITUDE_ERR," << tp.err << '\n';
-		std::cout << "PULSE_WIDTH,"	  << tp.width << '\n';
-	}
+    void SawAnalyzingFunc::test_param_out(const TEST_PARAM &tp)
+    {
+        std::cout << "TestParameters\n";
+        std::cout << "AMPLITUDE_MIN," << tp.aml_min << '\n';
+        std::cout << "AMPLITUDE_MAX," << tp.aml_max << '\n';
+        std::cout << "AMPLITUDE_ERR," << tp.err << '\n';
+        std::cout << "PULSE_WIDTH," << tp.width << '\n';
+    }
 }
--- LibSawAnalyzing/LibSawAnalyzing.h
+++ LibSawAnalyzing/LibSawAnalyzing.h
@@ -7,49 +7,48 @@ using namespace std;
 
 namespace SawAnalyzeFuncs
 {
-	class SawAnalyzingFunc
-	{
-	public:
-		enum AMPLITUDE_ERRORS
-		{
-			AMPL_NORM,
-			AMPL_TOO_LOW,
-			AMPL_TOO_HIGHT,
-			AMPL_OUT_BORDER
-		};
+    class SawAnalyzingFunc
+    {
+    public:
+        enum AMPLITUDE_ERRORS
+        {
+            AMPL_NORM,
+            AMPL_TOO_LOW,
+            AMPL_TOO_HIGHT,
+            AMPL_OUT_BORDER
+        };
 
-		struct RESULT
-		{
-			bool form_tst;
-			int  bad_sample_num;
+        struct RESULT
+        {
+            bool form_tst;
+            int  bad_sample_num;
 
-			AMPLITUDE_ERRORS ampl_test;
-			int aml_min_err;
-			int aml_max_err;
+            AMPLITUDE_ERRORS ampl_test;
+            int aml_min_err;
+            int aml_max_err;
 
-			bool widht_test;
-			int  width_err;
-		}result_t;
+            bool widht_test;
+            int  width_err;
+        }result_t;
 
-		struct TEST_PARAM
-		{
-			int aml_min;
-			int aml_max;
-			int err;
-			int width;
-		}test_parameter;
+        struct TEST_PARAM
+        {
+            int aml_min;
+            int aml_max;
+            int err;
+            int width;
+        }test_parameter;
 
-		static AMPLITUDE_ERRORS amplitude_checking(const TEST_PARAM &tp, RESULT &res, vector<int> &data);
-		static bool pulse_width_checking(const TEST_PARAM &tp, RESULT &res, vector<int> &data);
-		static bool form_checking(RESULT &res, vector<int> &data);
-		static bool check_all_tests(const TEST_PARAM &tp, RESULT &res, vector<int> &data);
+        static AMPLITUDE_ERRORS amplitude_checking(const TEST_PARAM &tp, RESULT &res, vector<int> &data);
+        static bool pulse_width_checking(const TEST_PARAM &tp, RESULT &res, vector<int> &data);
+        static bool form_checking(RESULT &res, vector<int> &data);
+        static bool check_all_tests(const TEST_PARAM &tp, RESULT &res, vector<int> &data);
 
-		static bool result_check(RESULT &res);
+        static bool result_check(RESULT &res);
 
-		static void result_out(RESULT &res);
-		static void test_param_out(const TEST_PARAM &tp);
-		
+        static void result_out(RESULT &res);
+        static void test_param_out(const TEST_PARAM &tp);
 
-		friend std::ostream& operator<<(std::ostream& out, const AMPLITUDE_ERRORS err);	
-	};
+        friend std::ostream& operator<<(std::ostream& out, const AMPLITUDE_ERRORS err);
+    };
 }
